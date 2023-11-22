63d4003a93a723510d1c0187571b0d0f2e340d9a
Alessandro Rossi <alessandro.rossi.max@gmail.com>
2023-11-23 00:14:10 +0100

    accuracy and predict function

---
+++ botnet/centralized_version/accuracy.py
@@ -0,0 +1,11 @@
+import numpy as np
+from predict import predict
+
+def accuracy (w, b, np_Xy):
+    correctly_classified = 0;
+    for element in np_Xy:
+        if(predict(w,b,element[0]) == element[1]):
+            correctly_classified += 1
+
+    accuracy = correctly_classified/np_Xy.size
+    return accuracy
---
+++ botnet/centralized_version/predict.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def predict(w, b, X):
+    # Compute the linear combination of the weights and the example
+    z = 0
+    for i in range(len(w)):
+        z += w[i] * X[i]
+
+    # Apply the sigmoid function to get the probability
+    p = 1 / (1 + np.exp(-z))
+
+    # Predict the class label (0 or 1) based on the probability
+    if p >= 0.5:
+        return 1
+    else:
+        return 0
