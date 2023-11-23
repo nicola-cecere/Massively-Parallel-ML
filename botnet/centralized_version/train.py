2f4d5cffa744feb7057db7b3c3f29a95156946ea
Luca Petracca <lucapetrh@gmail.com>
2023-11-23 11:32:39 +0100

    centralized train function

---
+++ botnet/centralized_version/train.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def train(np_Xy, iterations, learning_rate, lambda_reg):
+
+    # Extracting features (X) and labels (y) from the input array
+    X = np.array([example[:-1] for example in np_Xy])  # Extract features
+    y = np.array([example[-1] for example in np_Xy])   # Extract labels
+
+    # Number of examples and features
+    m, n = X.shape
+
+    np.random.seed(0) # Added for reproducibility
+    # Initializing weights and bias with random values
+    w = np.random.rand(n)
+    b = np.random.rand()
+
+    for i in range(iterations):
+        # Compute predictions
+        z = np.dot(X, w) + b
+        predictions = 1 / (1 + np.exp(-z))  # Applying the sigmoid function
+
+        # Cost function with L2 regularization
+        cost = (-1/m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
+        cost += (lambda_reg / (2*m)) * np.sum(w**2)
+
+        if i % 100 == 0:
+            print(f"Iteration {i}, Cost: {cost}")
+
+        # Gradient calculation
+        dw = (1/m) * np.dot(X.T, (predictions - y)) + (lambda_reg/m) * w
+        db = (1/m) * np.sum(predictions - y)
+
+        # Update weights and bias
+        w -= learning_rate * dw
+        b -= learning_rate * db
+
+    return w, b
+
+
