729ef4f3789358ca6ac0a63c029cbb79c09861c9
Luca Petracca <lucapetrh@gmail.com>
2023-11-23 12:52:29 +0100

    train function testing

--- botnet/centralized_version/main.py
+++ botnet/centralized_version/main.py
@@ -1,7 +1,10 @@
 from preprocess import readFile
+from train import train
 
 if __name__ == "__main__":
     # read data
     data = readFile("data/botnet_tot_syn_l.csv")
-    print(data)
     # standardize
+    # train
+    weights, bias = train(data, 10, 1.5, 0.05)
+
--- botnet/centralized_version/train.py
+++ botnet/centralized_version/train.py
@@ -15,26 +15,29 @@ def train(np_Xy, iterations, learning_rate, lambda_reg):
     b = np.random.rand()
 
     for i in range(iterations):
+
         # Compute predictions
         z = np.dot(X, w) + b
         predictions = 1 / (1 + np.exp(-z))  # Applying the sigmoid function
+
+        # Clipping predictions to fix errors
+        predictions = np.clip(predictions, 1e-10, 1-1e-10)
 
         # Cost function with L2 regularization
         cost = (-1/m) * np.sum(y * np.log(predictions) + (1 - y) * np.log(1 - predictions))
         cost += (lambda_reg / (2*k)) * np.sum(w**2)
 
-        if i % 100 == 0:
-            print(f"Iteration {i}, Cost: {cost}")
+        print(f"Iteration {i}, Cost: {cost}")
 
         # Gradient calculation
-        # dw = (1/m) * np.dot(X.T, (predictions - y)) + (lambda_reg/k) * w
-        # db = (1/m) * np.sum(predictions - y)
+        dw = (1/m) * np.dot(X.T, (predictions - y)) + (lambda_reg/k) * w
+        db = (1/m) * np.sum(predictions - y)
 
         # Gradient calculation with np.sum and for loop
-        dw = np.zeros(k)
-        for j in range(k):
-            dw[j] = (1/m) * np.sum((predictions - y) * X[:, j]) + (lambda_reg/k) * w[j]
-        db = (1/m) * np.sum(predictions - y)
+        # dw = np.zeros(k)
+        # for j in range(k):
+        #    dw[j] = (1/m) * np.sum((predictions - y) * X[:, j]) + (lambda_reg/k) * w[j]
+        # db = (1/m) * np.sum(predictions - y)
 
         # Update weights and bias
         w -= learning_rate * dw
