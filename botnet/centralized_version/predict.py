e7eea3858c131c77c38dd9f78bddc2da2cd0b8c4
Alessandro Rossi <alessandro.rossi.max@gmail.com>
2023-11-29 17:36:35 +0100

    Changed predict function using for loops rather than np.dot function

--- botnet/centralized_version/predict.py
+++ botnet/centralized_version/predict.py
@@ -1,8 +1,16 @@
 import numpy as np
 
 def predict(w, b, X):
-    # Compute the linear combination of the weights and the example
-    z = np.dot(w, X) + b
+    # Initialize the sum
+    z = 0
+    # Iterate over the weights and corresponding features
+    for i in range(len(w)):
+        z += w[i] * X[i]
+    # Add the bias term
+    z += b
+
+    # compact way to calculate it z = np.dot(w, X) + b
+
     # Apply the sigmoid function to get the probability
     p = 1 / (1 + np.exp(-z))
     # Predict the class label (0 or 1) based on the probability
