84cfefbd6bc8500db8abcfb081466d590f9eb462
Luca Petracca <lucapetrh@gmail.com>
2023-11-30 15:33:29 +0100

    bug fixes

--- botnet/centralized_version/predict.py
+++ botnet/centralized_version/predict.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def predict(w, b, X):
-<<<<<<< HEAD
 
     # Initialize z to 0
     z = 0
@@ -10,18 +9,6 @@ def predict(w, b, X):
         z += w[i] * X[i]  # Accumulate the product of each feature and its weight
     # Add the bias term to z
     z += b
-=======
-    # Initialize the sum
-    z = 0
-    # Iterate over the weights and corresponding features
-    for i in range(len(w)):
-        z += w[i] * X[i]
-    # Add the bias term
-    z += b
-
-    # compact way to calculate it z = np.dot(w, X) + b
-
->>>>>>> e7eea3858c131c77c38dd9f78bddc2da2cd0b8c4
     # Apply the sigmoid function to get the probability
     p = 1 / (1 + np.exp(-z))
     # Predict the class label (0 or 1) based on the probability
