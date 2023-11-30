78dc7d453dbe9fd80bc50575a46bce8035f0cbed
Luca Petracca <lucapetrh@gmail.com>
2023-11-30 15:12:54 +0100

    Centralized Predict and train functions without np.dot

--- botnet/centralized_version/predict.py
+++ botnet/centralized_version/predict.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def predict(w, b, X):
+<<<<<<< HEAD
+
+    # Initialize z to 0
+    z = 0
+    # Iterate over each feature and its corresponding weight
+    for i in range(len(w)):
+        z += w[i] * X[i]  # Accumulate the product of each feature and its weight
+    # Add the bias term to z
+    z += b
+=======
     # Initialize the sum
     z = 0
     # Iterate over the weights and corresponding features
@@ -11,6 +21,7 @@ def predict(w, b, X):
 
     # compact way to calculate it z = np.dot(w, X) + b
 
+>>>>>>> e7eea3858c131c77c38dd9f78bddc2da2cd0b8c4
     # Apply the sigmoid function to get the probability
     p = 1 / (1 + np.exp(-z))
     # Predict the class label (0 or 1) based on the probability
--- botnet/centralized_version/train.py
+++ botnet/centralized_version/train.py
@@ -17,7 +17,11 @@ def train(np_Xy, iterations, learning_rate, lambda_reg):
     for i in range(iterations):
 
         # Compute predictions
-        z = np.dot(X, w) + b
+        # z = np.dot(X, w) + b
+        z = np.zeros(m)  # Initialize z as a zero array of length m
+        for j in range(k):
+            z += w[j] * X[:, j]  # Correctly accumulate the sum of products
+        z += b  # Add the bias term to each element of z
         y_hat = 1 / (1 + np.exp(-z))  # Applying the sigmoid function
 
         # Cost function with L2 regularization
@@ -27,19 +31,17 @@ def train(np_Xy, iterations, learning_rate, lambda_reg):
         print(f"Iteration {i}, Cost: {cost}")
 
         # Gradient calculation
-        dw = (1/m) * np.dot(X.T, (y_hat - y)) + (lambda_reg/k) * w
-        db = (1/m) * np.sum(y_hat - y)
+        # dw = (1/m) * np.dot(X.T, (y_hat - y)) + (lambda_reg/k) * w
+        # db = (1/m) * np.sum(y_hat - y)
 
         # Gradient calculation with np.sum and for loop
-        # dw = np.zeros(k)
-        # for j in range(k):
-        #    dw[j] = (1/m) * np.sum((y_hat - y) * X[:, j]) + (lambda_reg/k) * w[j]
-        # db = (1/m) * np.sum(y_hat - y)
+        dw = np.zeros(k)
+        for j in range(k):
+            dw[j] = (1/m) * np.sum((y_hat - y) * X[:, j]) + (lambda_reg/k) * w[j]
+        db = (1/m) * np.sum(y_hat - y)
 
         # Update weights and bias
         w -= learning_rate * dw
         b -= learning_rate * db
 
     return w, b
-
-
