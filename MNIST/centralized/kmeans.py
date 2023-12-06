3fd0dce0e3b78e68017634d53e7d0b173f83f091
Nicola Cecere <nicola.cecere@mail.polimi.it>
2023-12-06 14:57:05 +0100

    starting serialKmeans

--- MNIST/centralized/kmeans.py
+++ MNIST/centralized/kmeans.py
@@ -1,2 +1,25 @@
+import numpy as np
+
+
+def initialize_centroids(data, K):
+    """
+    Initialize K centroids from a standard normal distribution.
+
+    :param data: Pandas DataFrame, the dataset from which feature dimensions are determined.
+    :param K: int, the number of centroids to initialize.
+    :return: numpy array, initialized centroids.
+    """
+    # Number of features in the data
+    num_features = data.shape[1]
+
+    # Initialize centroids
+    centroids = np.random.randn(K, num_features)
+
+    return centroids
+
+
 def serialKMeans(X, K, n_iter):
+    # Initialize centroids
+    centroids = initialize_centroids(X, K)
+
     return
